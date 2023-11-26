f3f8ded10c4d7d734a5efa091abfdb1f5394317a
sailajalakkakula <123012535+sailajalakkakula@users.noreply.github.com>
2023-11-25 21:56:29 -0600

    first commit

---
+++ src/main/java/com/FellaClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.FellaClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author Sailaja Lakkakula
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
